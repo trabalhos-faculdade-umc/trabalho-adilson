5af9c52ccb39e65e4968430b8b848f4029fc3253
Juan Pablo <juanpneres15@gmail.com>
2024-11-17 14:05:53 -0300

    Criando atrib e métodos

--- src/java/model/Pesquisa.java
+++ src/java/model/Pesquisa.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
 import java.util.Date;
@@ -13,78 +9,40 @@ public class Pesquisa {
     // Atrib
     private int id;
     private String nome;
-    private String email;
-    private String dt_pesq;
-    private int q1;
-    private int q2;
-    private int q3;
-    private int q4;           
+    private String tipo;           
+    private int quant;
     
     //Métodos
+    
+    public int getId() {
+        return id;
+    }
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public String getTipo() {
+        return tipo;
     }
 
-    public void setDt_pesq(String dt_pesq) {
-        this.dt_pesq = dt_pesq;
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
     }
 
-    public void setQ1(int q1) {
-        this.q1 = q1;
+    public int getQuant() {
+        return quant;
     }
 
-    public void setQ2(int q2) {
-        this.q2 = q2;
-    }
-
-    public void setQ3(int q3) {
-        this.q3 = q3;
-    }
-
-    public void setQ4(int q4) {
-        this.q4 = q4;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    
-    public String getNome() {
-        return nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getDt_pesq() {
-        return dt_pesq;
-    }
-
-    public int getQ1() {
-        return q1;
-    }
-
-    public int getQ2() {
-        return q2;
-    }
-
-    public int getQ3() {
-        return q3;
-    }
-
-    public int getQ4() {
-        return q4;
-    }
-        
+    public void setQuant(int quant) {
+        this.quant = quant;
+    }       
 }
