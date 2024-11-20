8094bce36c550186b428c2c24164464b85ac2ae4
Juan Pablo <juanpneres15@gmail.com>
2024-11-20 13:44:45 -0300

    Adicionando mais atributos

--- src/java/model/Pesquisa.java
+++ src/java/model/Pesquisa.java
@@ -11,6 +11,10 @@ public class Pesquisa {
     private String nome;
     private String tipo;           
     private int quant;
+    private int q1;
+    private int q2;
+    private int q3;
+    private int q4;
     
     //Métodos
     
@@ -44,5 +48,38 @@ public class Pesquisa {
 
     public void setQuant(int quant) {
         this.quant = quant;
-    }       
+    }
+
+    public int getQ1() {
+        return q1;
+    }
+
+    public void setQ1(int q1) {
+        this.q1 = q1;
+    }
+
+    public int getQ2() {
+        return q2;
+    }
+
+    public void setQ2(int q2) {
+        this.q2 = q2;
+    }
+
+    public int getQ3() {
+        return q3;
+    }
+
+    public void setQ3(int q3) {
+        this.q3 = q3;
+    }
+
+    public int getQ4() {
+        return q4;
+    }
+
+    public void setQ4(int q4) {
+        this.q4 = q4;
+    }
+    
 }
