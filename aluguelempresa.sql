bd4b778e8889a6eb2652b8ec519d14bd30b1d40d
Gustavo Silva <113210648+gustavo-log@users.noreply.github.com>
2024-11-28 20:06:45 -0300

    ADD: SQL file

--- aluguelempresa.sql
+++ aluguelempresa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2024 at 11:59 PM
+-- Generation Time: Nov 29, 2024 at 12:05 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
